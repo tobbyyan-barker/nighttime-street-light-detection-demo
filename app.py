8f76bbc4514ec21e20084e047e90d8d614783245
tobbyyan-barker <yanyishun11@gmail.com>
2026-05-15 17:20:41 +0800

    Add video detection demo with frame extraction

--- app.py
+++ app.py
@@ -91,6 +91,147 @@ def predict():
         "detections": detections
     })
 
+@app.route("/predict_video", methods=["POST"])
+def predict_video():
+    #检查这个"video"字段是否在前端传过来的formData里面
+    if "video" not in request.files:
+        return jsonify({"error": "no video file upload"}),400
+    #前端传递的字段是"video" 后端用request files接收视频
+    video = request.files["video"]
+    # 看一下视频的文件名是否为空字符串 为空字符串 不保存 返回前端叫用户命名:
+    # 空字符串: "" 空格: " "
+    if video.filename == "":
+        return jsonify({"error": "no selected files"}),400
+    
+    #后端建立保存视频的文件夹 os.makedirs建立保存视频的文件夹
+    #路径也要加引号 也是字符串
+    upload_dir = "static/uploads/videos"
+    os.makedirs(upload_dir,exist_ok=True)
+    
+    #视频的实际路径 = 视频文件夹 + 图片的名字 成功保存视频
+    video_path= os.path.join(upload_dir,video.filename)
+    video.save(video_path)
+
+    #创建抽帧保存文件夹 可以给YOLO模型读取
+    frame_dir = "static/frames"
+    os.makedirs(frame_dir,exist_ok=True)
+
+    #打开视频 (用Opencv的capture函数)
+    capture = cv2.VideoCapture(video_path)
+
+    # 安全措施: 判断视频是否能打开
+    if not capture.isOpened():
+        return jsonify({"error": "can't open this video"}),400
+
+    #获得原始视频的特性: 比如30FPS表示 每1s 30帧 因为cv2只认帧数 不会认秒数 为了把秒数转换成帧数
+    original_frame = capture.get(cv2.CAP_PROP_FPS) 
+
+    # 防止 FPS 读取失败
+    if original_frame <= 0:
+        original_frame = 30
+
+    #这个人为更改 每几秒抽帧一帧 换成帧--- 每60帧里面抽一帧  
+    fps_interval = 1 # 每一秒抽一帧
+
+    #这个是计算每多少帧里面抽一帧 --- 把秒---帧
+    frame_interval = max(1, int(original_frame * fps_interval))
+
+    #然后循环从视频里抽取帧 (cv2只能一帧一帧按顺序抽取)
+    frame_id = 0  #便于标记原始视频对应的帧
+    save_id = 0 # 标记我们要存储的帧的编号
+    saved_list = [] #便于存储我们需要的图片/帧的路径
+
+    #无限循环 这个次数不好判断 用while
+    #循环作用: 逐帧读取视频
+    while True:
+        # 这句的话 就是从视频里一帧一帧的读 返回是否读到帧(ret变量) 以及当前对应的frame
+       ret, frame = capture.read()
+       #当ret不是1 即ret 不是true的时候 说明循环停止
+       if not ret:
+          break
+
+       #表示每个frame_interval取一帧
+       if frame_id % frame_interval ==0:
+          #save_id 直接对应当前帧的id
+          save_id = frame_id
+          #frame的路径是static/frame/frame_0001.jpg (我是想写这个)
+          #:04d 这里的: 表示的是至少占4位 不足补0
+          #f"{1:.4d}" --- 不规范 只是碰巧能跑 这种写法的.4d 只是保留小数点后4位 适合浮点数用 整数不支持这个写法
+          frame_path = os.path.join(frame_dir,f"frame_{save_id:04d}.jpg")
+          #保存图片到frame_path路径下
+          #注意: 这个frame是opencv读出来的图像数组，不是flask上传上来的文件对象
+          #所以不能用.save()
+          #cv2.imwrite(frame_path,frame) --- 保存openCV读取出来的一帧图片
+          cv2.imwrite(frame_path,frame)
+          #保存这个路径到static/frame文件夹里
+          saved_list.append(frame_path)
+
+       frame_id += 1
+    
+    #用完释放资源
+    capture.release()
+
+    # 读取前端传来的置信度阈值，默认 0.5 (如果前端传了conf,用前端的 没传，默认0.5)
+    conf = float(request.form.get("conf", 0.5))
+
+    #新建保存模型画框后的图片的文件夹
+    result_dir = "static/results/video_frames"
+    os.makedirs(result_dir,exist_ok=True)
+
+    # 提取所有抽帧图片的检测信息 以及检测框的数量
+    all_results = []
+    total_count = 0
+
+    # YOLO 推理 然后把每一张在saved_list里面保存的图片喂给YOLO 让YOLO跑预测
+    for frame_path in saved_list:
+        # yolo 推理
+        results = model.predict(source=frame_path,conf=conf,save=False)
+        #提取结果图
+        result = results[0]
+        # 生成画框后的图片 result.plot() 会返回画框后的图片
+        plotted_img = result.plot()
+        #生成result_name, 后面生成result_path时要用
+        #os.path.basename表示的时提取frame_path最后一部分文件名
+        result_name = os.path.basename(frame_path)
+        result_path = os.path.join(result_dir,result_name)
+
+        # 保存检测后的图片
+        cv2.imwrite(result_path,plotted_img)
+
+        # 提取每张抽帧图片的检测信息
+        detections = []
+        #提取检测信息 一张图片可能有很多个预测框 都要提取出来
+        for box in result.boxes:
+            cls_id = int(box.cls[0])
+            class_name = model.names[cls_id]
+            confidence = float(box.conf[0])
+            detections.append({
+                "class_name": class_name,
+                "confidence": round(confidence, 3)
+            })
+        
+        box_count = len(detections)
+        total_count += box_count
+
+        all_results.append({
+            "frame_path": "/" + frame_path,
+            "result_path": "/" + result_path,
+            "count": box_count,
+            "detections": detections
+        })
+
+    #返回所有帧的结果
+    return jsonify({
+        "message": "video processed successfully",
+        "video_path": "/" + video_path,
+        "fps": original_frame,
+        "frame_interval": frame_interval,
+        "frame_count": len(saved_list),
+        #预测框的数量
+        "count": total_count,
+        "results": all_results
+    })
+
 #启动flask服务 debug=true --- 调试模式 debug=false (开发模式)
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,49 +1,69 @@
 const imageInput = document.getElementById("imageInput");
+const videoInput = document.getElementById("videoInput");
+
 const detectBtn = document.getElementById("detectBtn");
+const detectVideoBtn = document.getElementById("detectVideoBtn");
+
 const originalPreview = document.getElementById("originalPreview");
 const resultPreview = document.getElementById("resultPreview");
+
 const statusText = document.getElementById("statusText");
 const countText = document.getElementById("countText");
 const resultList = document.getElementById("resultList");
+
 const confInput = document.getElementById("confInput");
 const confValue = document.getElementById("confValue");
 
-let selectedFile = null;
+const frameCountText = document.getElementById("frameCountText");
+const videoCountText = document.getElementById("videoCountText");
+const videoGallery = document.getElementById("videoGallery");
 
-//置信度滑动条拖动逻辑: 置信度条被拖动--- 触发input 然后这里读取confInput.vale的值 并保留两位小数 最后html更新这个值
+let selectedFile = null;
+let selectedVideoFile = null;
+
+// 置信度滑动条拖动逻辑
 confInput.addEventListener("input", () => {
   confValue.textContent = Number(confInput.value).toFixed(2);
 });
 
-//图片上传和预览 这一整块负责：用户选择图片后, 显示原图预览，并重置旧结果
-//4.1: 监听用户选择图片: 当用户选择图片后，触发change事件，执行里面的代码
+// 图片上传和预览
 imageInput.addEventListener("change", () => {
-  // 取出第一张上传的图片 这里只上传一张 只取第一张
   selectedFile = imageInput.files[0];
-  // selectFile 表示当前用户上传的图片 后面点击检测按钮时，会用这个selectFile 上传给flask
+
   if (!selectedFile) {
     return;
   }
 
-  // 生成原图预览地址： 给本地图片临时生成一个浏览器可以显示的URL
   const imageURL = URL.createObjectURL(selectedFile);
-  
+
   originalPreview.src = imageURL;
-  // 把上传图片显示出来
   originalPreview.style.display = "block";
 
-  // 清空上一轮检测结果图
   resultPreview.src = "";
   resultPreview.style.display = "none";
 
-  // 重置检测数量和列表
-  countText.textContent = "0";  // 改文字
-  resultList.innerHTML = "";    
+  countText.textContent = "0";
+  resultList.innerHTML = "";
 
-  // 更新状态提示
   statusText.textContent = "Image selected. Ready to detect.";
 });
 
+// 视频上传逻辑
+videoInput.addEventListener("change", () => {
+  selectedVideoFile = videoInput.files[0];
+
+  if (!selectedVideoFile) {
+    return;
+  }
+
+  frameCountText.textContent = "0";
+  videoCountText.textContent = "0";
+  videoGallery.innerHTML = "";
+
+  statusText.textContent = "Video selected. Ready to detect.";
+});
+
+// 图片检测按钮
 detectBtn.addEventListener("click", async () => {
   if (!selectedFile) {
     alert("Please choose an image first.");
@@ -54,38 +74,27 @@ detectBtn.addEventListener("click", async () => {
   formData.append("image", selectedFile);
   formData.append("conf", confInput.value);
 
-  //更新状态为检测中
-  statusText.textContent = "Detecting... Please wait.";
+  statusText.textContent = "Detecting image... Please wait.";
 
-  // try...catch 处理正常检测和错误(异常检测)
-  // try 里面放入任何可能出错的代码 中间有一步出错 就进入catch  显示错误信息
   try {
-    // fetch 调用flask后端
     const response = await fetch("/predict", {
       method: "POST",
       body: formData
     });
 
-    //接收flask 返回的json数据
     const data = await response.json();
-    // 这几行是调试用的
-    console.log(data);
-    console.log(data.count)
-    console.log(data.message)
 
-    // 检测后端是否相应成功
+    console.log(data);
+
     if (!response.ok) {
-      throw new Error(data.error || "Detection failed.");
+      throw new Error(data.error || "Image detection failed.");
     }
 
-    // 显示检测结果图
     resultPreview.src = data.result_image + "?t=" + new Date().getTime();
     resultPreview.style.display = "block";
 
-    // 更新检测数量
     countText.textContent = data.count;
 
-    // 清空旧的结果列表 每次新检测前，先把旧结果列表清空
     resultList.innerHTML = "";
 
     if (data.detections.length === 0) {
@@ -98,8 +107,91 @@ detectBtn.addEventListener("click", async () => {
       });
     }
 
-    statusText.textContent = "Detection completed.";
+    statusText.textContent = "Image detection completed.";
   } catch (error) {
     statusText.textContent = "Error: " + error.message;
   }
 });
+
+// 视频检测按钮
+detectVideoBtn.addEventListener("click", async () => {
+  if (!selectedVideoFile) {
+    alert("Please choose a video first.");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("video", selectedVideoFile);
+  formData.append("conf", confInput.value);
+
+  statusText.textContent = "Detecting video... This may take a while.";
+
+  frameCountText.textContent = "0";
+  videoCountText.textContent = "0";
+  videoGallery.innerHTML = "";
+
+  try {
+    const response = await fetch("/predict_video", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!response.ok) {
+      throw new Error(data.error || "Video detection failed.");
+    }
+
+    frameCountText.textContent = data.frame_count;
+    videoCountText.textContent = data.count;
+
+    if (!data.results || data.results.length === 0) {
+      videoGallery.innerHTML = "<p>No frames were extracted from this video.</p>";
+      statusText.textContent = "Video detection completed, but no frame was extracted.";
+      return;
+    }
+
+    data.results.forEach((item, index) => {
+      const card = document.createElement("div");
+      card.className = "video-frame-card";
+
+      const img = document.createElement("img");
+      img.src = item.result_path + "?t=" + new Date().getTime();
+      img.alt = `Video detection result frame ${index + 1}`;
+
+      const title = document.createElement("p");
+      title.innerHTML = `<strong>Frame ${index + 1}</strong>`;
+
+      const count = document.createElement("p");
+      count.textContent = `Detected objects: ${item.count}`;
+
+      const details = document.createElement("p");
+
+      if (!item.detections || item.detections.length === 0) {
+        details.textContent = "No object detected.";
+      } else {
+        const detectionText = item.detections
+          .map((det, detIndex) => {
+            return `${detIndex + 1}. ${det.class_name} (${det.confidence})`;
+          })
+          .join(" | ");
+
+        details.textContent = detectionText;
+      }
+
+      card.appendChild(img);
+      card.appendChild(title);
+      card.appendChild(count);
+      card.appendChild(details);
+
+      videoGallery.appendChild(card);
+    });
+
+    statusText.textContent = "Video detection completed.";
+  } catch (error) {
+    statusText.textContent = "Error: " + error.message;
+  }
+});
+
